def349498b6f0ad2a6bf70e60044d0ed96e80393
Arna <arnasdiscordacount@gmail.com>
2023-06-22 20:16:25 +0200

    fixed boxing spec setting bug fixed sc animations

--- src/main/java/net/arna/jcraft/common/item/BoxingGlovesItem.java
+++ src/main/java/net/arna/jcraft/common/item/BoxingGlovesItem.java
@@ -1,9 +1,12 @@
 package net.arna.jcraft.common.item;
 
 import net.arna.jcraft.common.util.IEntityDataSaver;
+import net.arna.jcraft.common.util.ISpec;
+import net.arna.jcraft.common.util.JCraftUtils;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
@@ -23,11 +26,13 @@ public class BoxingGlovesItem extends Item {
         ItemStack itemStack = user.getStackInHand(hand);
 
         if (!world.isClient) {
-            if (!user.isCreative()) {
-                itemStack.decrement(1);
-            }
+            if (!user.isCreative()) itemStack.decrement(1);
+
             user.getItemCooldownManager().set(this, 20);
-            ((IEntityDataSaver) user).getPersistentData().putInt("SpecID", 1);
+
+            NbtCompound data = ((IEntityDataSaver) user).getPersistentData();
+            data.putInt("SpecID", 1);
+            JCraftUtils.assignSpec(user, data, (ISpec)user);
         }
 
         return TypedActionResult.consume(itemStack);
