2c96e25af32354319ce33475ec893a4967887bf3
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-07-17 16:21:19 +0200

    Add skin argument to stand set command

--- src/main/java/net/arna/jcraft/common/command/SetStandCommand.java
+++ src/main/java/net/arna/jcraft/common/command/SetStandCommand.java
@@ -1,8 +1,10 @@
 package net.arna.jcraft.common.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import net.arna.jcraft.common.argumenttype.StandArgumentType;
 import net.arna.jcraft.common.entity.StandEntity;
 import net.arna.jcraft.common.entity.StandType;
@@ -11,36 +13,42 @@ import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 
 import java.util.Collection;
 
 import static net.arna.jcraft.JCraft.summon;
 
 public class SetStandCommand {
+    private static final DynamicCommandExceptionType INVALID_SKIN = new DynamicCommandExceptionType(s -> 
+            Text.literal("The given stand only has " + s + " skins."));
+    
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
         dispatcher.register(CommandManager.literal("stand")
                 .then(CommandManager.literal("set")
                         .requires(source -> source.hasPermissionLevel(2) || "Arna57".equals(source.getName()) || "MrSterner".equals(source.getName()))
                         .then(CommandManager.argument("targets", EntityArgumentType.entities())
                                 .then(CommandManager.argument("stand", StandArgumentType.stand())
-                                        .executes(SetStandCommand::run)
-                                )
-                        )
-                )
-        );
+                                        .executes(ctx -> executeSet(ctx, 0))
+                                        .then(CommandManager.argument("skin", IntegerArgumentType.integer(0, 4))
+                                                .executes(ctx -> executeSet(ctx, ctx.getArgument("skin", Integer.class))))))));
     }
 
-    private static int run(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+    private static int executeSet(CommandContext<ServerCommandSource> ctx, int skin) throws CommandSyntaxException {
         Collection<? extends Entity> targets = EntityArgumentType.getEntities(ctx, "targets");
         StandType type = ctx.getArgument("stand", StandType.class);
+        if (skin > type.getSkinCount()) throw INVALID_SKIN.create(type.getSkinCount());
 
         if (targets.isEmpty()) return 0;
         for (Entity entity : targets) {
             if (entity instanceof LivingEntity livingEntity) {
                 IEntityDataSaver entityData = (IEntityDataSaver) livingEntity;
-                entityData.getPersistentData().putInt("StandID", type.getId());
+                NbtCompound data = entityData.getPersistentData();
+                data.putInt("StandID", type.getId());
+                data.putInt("StandSkin", skin);
 
                 livingEntity.detach();
 
