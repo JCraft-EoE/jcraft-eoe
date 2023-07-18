21dfa5d84514326ecc1cc2bdca597b601d5a2021
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-07-18 11:52:48 +0200

    Check if the item is a disc first

--- src/main/java/net/arna/jcraft/common/item/StandDiscItem.java
+++ src/main/java/net/arna/jcraft/common/item/StandDiscItem.java
@@ -2,6 +2,7 @@ package net.arna.jcraft.common.item;
 
 import net.arna.jcraft.common.entity.StandType;
 import net.arna.jcraft.common.util.IEntityDataSaver;
+import net.arna.jcraft.registry.JObjectRegistry;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -27,7 +28,7 @@ public class StandDiscItem extends Item {
             TextColor.fromFormatting(Formatting.LIGHT_PURPLE)
     };
     private static final Text DEFAULT_SKIN = Text.literal("Default");
-    
+
     public StandDiscItem(Settings settings) {
         super(settings);
     }
@@ -40,7 +41,7 @@ public class StandDiscItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         if (world.isClient) return TypedActionResult.pass(itemStack);
-        
+
         if (user.getDamageTracker().wasRecentlyAttacked()) {
             user.sendMessage(Text.translatable("jcraft.disc.error"));
             return TypedActionResult.fail(itemStack);
@@ -56,7 +57,7 @@ public class StandDiscItem extends Item {
         int userSkin = 0;
 
         IEntityDataSaver userDataSaver = ((IEntityDataSaver) user);
-        
+
         NbtCompound data = itemStack.getOrCreateNbt();
         NbtCompound userData = userDataSaver.getPersistentData();
 
@@ -80,27 +81,33 @@ public class StandDiscItem extends Item {
         StandType type = getStandType(stack);
         if (type == null) return;
         tooltip.add(type.getNameText().copy().styled(s -> s.withColor(type.isEvolution() ? Formatting.LIGHT_PURPLE : Formatting.GRAY)));
-        
+
         int skin = getSkin(stack);
         tooltip.add((skin == 0 || skin > type.getSkinCount() ? DEFAULT_SKIN : type.getSkinNames().get(skin - 1)).copy()
                 .styled(s -> s.withColor(SKIN_LEVEL_COLORS[skin])));
     }
-    
+
     public static boolean isEmptyDisc(ItemStack stack) {
         return stack.getNbt() == null || !stack.getNbt().contains("StandID", NbtElement.INT_TYPE);
     }
-    
+
     public static StandType getStandType(ItemStack stack) {
+        if (!stack.isOf(JObjectRegistry.STANDDISC)) return null;
+
         NbtCompound nbt = stack.getNbt();
         return nbt == null || !nbt.contains("StandID", NbtElement.INT_TYPE) ? null : StandType.fromId(nbt.getInt("StandID"));
     }
-    
+
     public static void setSkin(ItemStack stack, int skin) {
+        if (!stack.isOf(JObjectRegistry.STANDDISC)) return;
+
         NbtCompound nbt = stack.getOrCreateNbt();
         nbt.putInt("Skin", skin);
     }
-    
+
     public static int getSkin(ItemStack stack) {
+        if (!stack.isOf(JObjectRegistry.STANDDISC)) return 0;
+
         NbtCompound nbt = stack.getNbt();
         return nbt == null || !nbt.contains("Skin", NbtElement.INT_TYPE) ? 0 : nbt.getInt("Skin");
     }
