5f6d8f24903deeba1e2bd11d0408971475f32f24
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-08-10 22:10:06 +0200

    Override tick-delta when in time-stop

--- src/client/java/net/arna/jcraft/client/mixin/BlockEntityRenderDispatcherMixin.java
+++ src/client/java/net/arna/jcraft/client/mixin/BlockEntityRenderDispatcherMixin.java
@@ -1,16 +1,19 @@
 package net.arna.jcraft.client.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.arna.jcraft.client.util.JClientUtils;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(BlockEntityRenderDispatcher.class)
 public class BlockEntityRenderDispatcherMixin {
-    //todo: disable deltaTick for blocks caught in timestop
-    private void jcraft$deltaTickBlock(Args args) {
-        BlockEntity blockEntity = args.get(0);
-        if (JClientUtils.getTicksIfInTSRange(blockEntity.getPos()) > 0) args.set(1, 0.0F); // Args 0 = blockent, 1 = deltatick
+
+    @ModifyVariable(method = "render(Lnet/minecraft/client/render/block/entity/BlockEntityRenderer;Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V",
+            at = @At("HEAD"), argsOnly = true)
+    private static float overrideTickDeltaIfInTimeStop(float tickDelta, @Local BlockEntity blockEntity) {
+        return JClientUtils.getTicksIfInTSRange(blockEntity.getPos()) > 0 ? 0 : tickDelta;
     }
 }
