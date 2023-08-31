b20d8900876686f949cb61d07329d70ff49746a8
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-08-31 23:50:00 +0200

    Fix barrage attacks performing before their windup has passed

--- src/main/java/net/arna/jcraft/common/attack/moves/base/AbstractBarrageAttack.java
+++ src/main/java/net/arna/jcraft/common/attack/moves/base/AbstractBarrageAttack.java
@@ -50,7 +50,7 @@ public abstract class AbstractBarrageAttack<T extends AbstractBarrageAttack<T, A
         // Which means that if your move stun is 22, windup is 6 and interval is 6,
         // the first blow will not be landed after 6 ticks (when stand move stun is 22 - 6 = 16),
         // but rather after 10 ticks (when stand move stun is 22 - 10 = 12).
-        return attacker.hasUser() && (getDuration() - getWindup() - attacker.getMoveStun()) % interval == 0;
+        return attacker.hasUser() && hasWindupPassed(attacker) && (getDuration() - getWindup() - attacker.getMoveStun()) % interval == 0;
     }
 
     @Override
