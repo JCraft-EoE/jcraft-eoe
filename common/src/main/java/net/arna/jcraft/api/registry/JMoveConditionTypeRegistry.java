ab2816582c9ba7c2c825f09a2542dc0f07caff57
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2026-04-17 11:08:21 +0200

    Add Hamon move conditions to registry

--- common/src/main/java/net/arna/jcraft/api/registry/JMoveConditionTypeRegistry.java
+++ common/src/main/java/net/arna/jcraft/api/registry/JMoveConditionTypeRegistry.java
@@ -15,6 +15,13 @@ public interface JMoveConditionTypeRegistry {
     RegistrySupplier<MoveConditionType<?>> METALLICA_IRON = register("metallica_iron", MetallicaIronCondition.Type.INSTANCE);
     RegistrySupplier<MoveConditionType<?>> HOLDING_ANUBIS = register("holding_anubis", HoldingAnubisCondition.Type.INSTANCE);
     RegistrySupplier<MoveConditionType<?>> HAMON_CHARGE = register("hamon_charge", HamonChargeCondition.Type.INSTANCE);
+    // TODO these are all basically the same and we do not need one for each move. The lessonTime parameter should suffice.
+    RegistrySupplier<MoveConditionType<?>> HAMON_BREATH = register("hamon_breath", HamonBreathCondition.Type.INSTANCE);
+    RegistrySupplier<MoveConditionType<?>> HAMON_OVERDRIVE = register("hamon_overdrive", HamonOverdriveCondition.Type.INSTANCE);
+    RegistrySupplier<MoveConditionType<?>> HAMON_SENDO_WAVE_KICK_AERIAL = register("hamon_sendo_wave_kick_aerial", HamonSendoWaveKickAerialCondition.Type.INSTANCE);
+    RegistrySupplier<MoveConditionType<?>> HAMON_SENDO_WAVE_KICK_GROUNDED = register("hamon_sendo_wave_kick_grounded", HamonSendoWaveKickGroundedCondition.Type.INSTANCE);
+    RegistrySupplier<MoveConditionType<?>> HAMON_WAVE = register("hamon_wave", HamonWaveCondition.Type.INSTANCE);
+    RegistrySupplier<MoveConditionType<?>> HAMON_ZOOM_PUNCH = register("hamon_zoom_punch", HamonZoomPunchCondition.Type.INSTANCE);
 
     private static RegistrySupplier<MoveConditionType<?>> register(String name, MoveConditionType<?> type) {
         return MOVE_CONDITION_TYPE_REGISTRY.register(name, () -> type);
