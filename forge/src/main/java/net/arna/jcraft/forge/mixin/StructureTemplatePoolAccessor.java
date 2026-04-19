dfd1dde5325b3575265057222f5a5342f6b76a47
Ayutac <fly.high.android@gmail.com>
2026-04-19 04:46:43 +0200

    Tried to add an addition to Forge, my bad.

---
+++ forge/src/main/java/net/arna/jcraft/forge/mixin/StructureTemplatePoolAccessor.java
@@ -0,0 +1,22 @@
+package net.arna.jcraft.forge.mixin;
+
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(StructureTemplatePool.class)
+public interface StructureTemplatePoolAccessor {
+
+    @Accessor
+    List<Pair<StructurePoolElement, Integer>> getRawTemplates();
+
+    @Accessor
+    @Mutable
+    void setRawTemplates(List<Pair<StructurePoolElement, Integer>> list);
+
+}
