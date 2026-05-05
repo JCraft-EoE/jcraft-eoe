0105efce8eaf31a780028128751754b462d57583
Arna577 <arnasdiscordacount@gmail.com>
2026-05-05 23:20:26 +0200

    fix stand wall suffocation

--- common/src/main/java/net/arna/jcraft/api/stand/StandEntity.java
+++ common/src/main/java/net/arna/jcraft/api/stand/StandEntity.java
@@ -175,6 +175,7 @@ public abstract class StandEntity<E extends StandEntity<E, S>, S extends Enum<S>
 
         standType = type;
         noCulling = true;
+        noPhysics = true; // immune to suffocation
 
         assert getThis() == this;
     }
