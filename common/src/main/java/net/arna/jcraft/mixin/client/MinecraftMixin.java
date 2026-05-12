3da8917221e05133d01f5279391964cf11549537
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2026-05-12 22:26:13 +0200

    Ignore pick block when stand is out

--- common/src/main/java/net/arna/jcraft/mixin/client/MinecraftMixin.java
+++ common/src/main/java/net/arna/jcraft/mixin/client/MinecraftMixin.java
@@ -1,7 +1,10 @@
 package net.arna.jcraft.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.platform.Window;
 import net.arna.jcraft.client.rendering.api.callbacks.DisplayResizeCallback;
+import net.arna.jcraft.common.util.JUtils;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,5 +20,11 @@ public class MinecraftMixin {
     @Inject(method = "resizeDisplay", at = @At("RETURN"))
     private void onResizeDisplay(final CallbackInfo ci) {
         DisplayResizeCallback.EVENT.invoker().onResolutionChanged(window.getWidth(), window.getHeight());
+    }
+
+    @WrapOperation(method = "handleKeybinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;pickBlock()V"))
+    private void ignorePickBlockWhenStandOut(Minecraft instance, Operation<Void> original) {
+        if (JUtils.getStand(instance.player) == null)
+            original.call(instance);
     }
 }
